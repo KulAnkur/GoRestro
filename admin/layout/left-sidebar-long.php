f16e9505d50222170641443f4c6c280fb54288ba
Ankur Kulkarni <87350643+KulAnkur@users.noreply.github.com>
2022-02-19 08:14:28 +0530

    Add files via upload

---
+++ admin/layout/left-sidebar-long.php
@@ -0,0 +1,16 @@
+<div class="row">
+			
+			<div class="col s2 left-sidebar" id="left-sidebar" style="background: #2F4F4F;">
+
+				<div class="row">
+					<div class="col">
+						<h4 class="heading-name">Go Restro</h4>
+					</div>
+				</div>
+				<ul class="nav nav-sidebar list-group" >
+				    <li class="list-group-item active" style="background: #A9A9A9;"><a href="food-list.php">Foods </a></li>
+				    <li class="list-group-item" style="background: #A9A9A9;"><a href="category-list.php">Category</a></li>
+				    <li class="list-group-item" style="background: #A9A9A9;"><a href="order-list.php">Orders</a></li>
+				    <li class="list-group-item modal-trigger" data-target="modal1" style="background: #A9A9A9;"><a href="#">About</a></li>
+				  </ul>
+			</div>
---
+++ chunks/footer.php
@@ -0,0 +1,28 @@
+<section class="ffooter">
+		<footer class="page-footer">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">Contact US</h5>
+                <p class="grey-text text-lighten-4">JP Road, Goreagaon West, Mumbai</p>
+                <p class="grey-text text-lighten-4">Phone : ++22 254515487552</p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Social Media Links</h5>
+                <ul>
+                  <li><a class="grey-text text-lighten-3" href="#!">Facebook</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Instagram</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Twitter</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Whatsapp</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            © 2021 Copyright @ Go Restro
+            <a class="grey-text text-lighten-4 right" href="#!">Made in India </span></a>
+            </div>
+          </div>
+        </footer>
+	</section>
